605aeca2b7dc5d1034b99a3f481576667b211710
Dr-chen99 <dakaizhe@gmail.com>
2023-08-30 20:04:00 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,11 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git diskman https://github.com/jjm2473/luci-app-diskman.git;dev' >> feeds.conf.default
+echo 'src-git sbwml https://github.com/sbwml/openwrt_helloworld' >>feeds.conf.default
+echo 'src-git cups https://github.com/gdck/luci-app-cupsd.git' >>feeds.conf.default
+echo 'src-git backup https://github.com/Dr-chen99/backup-package' >>feeds.conf.default
+./scripts/feeds update -a
+./scripts/feeds install -a
--- diy-part2.sh
+++ diy-part2.sh
@@ -13,4 +13,14 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.21/g' package/base-files/files/bin/config_generate
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile   # 选择argon为默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile   # 选择argon为默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile   # 选择argon为默认主题
+sed -i 's/LUCI_DEPENDS:=+umbim/#LUCI_DEPENDS:=+umbim/g' feeds/luci/protocols/luci-proto-mbim/Makefile   #暂时不编译进入
+sed -i 's/LUCI_DEPENDS:=+uqmi/#LUCI_DEPENDS:=+uqmi/g' feeds/luci/protocols/luci-proto-qmi/Makefile  #暂时不编译进入
+rm -rf feeds/packages/lang/golang   #删除自带
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+rm -rf package/diy/luci-app-ota
+git clone -b main https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
+git clone -b main https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
